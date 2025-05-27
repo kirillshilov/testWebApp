02636407eb3b13db290b56da6f1ce46b158b0e2c
Kirill Shilov <sarataza@inbox.ru>
2025-05-27 09:53:46 +0300

    init

---
+++ src/test/java/com/webapptest/webapptest/WebAppTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapptest.webapptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
